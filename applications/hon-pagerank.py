1188851b7d6b583cde98bb772e5163b88ccb0699
Jungho Bang <me@bangtoven.com>
2018-04-11 02:01:18 -0400

    change the convergence condition

--- applications/hon-pagerank.py
+++ applications/hon-pagerank.py
@@ -51,7 +51,7 @@ for testSet in test_plists:
         tempDict[key] = 1
 
     print('converting pr')
-    pr = nx.pagerank(G, alpha=0.85, personalization = tempDict, weight = 'weight', tol=1e-09, max_iter=1000)
+    pr = nx.pagerank(G, alpha=0.85, personalization = tempDict, weight = 'weight', tol=1e-06, max_iter=500)
     print('pr converted')
 
     RealPR = {}
